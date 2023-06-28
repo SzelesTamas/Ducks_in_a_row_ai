f5692621de9f5e5a66ad20e1a91334f7ae8ded6a
SzelesTamas <1szelestamas@gmail.com>
2023-06-28 20:41:34 +0200

    Saving functionality for the networks

--- v3/neural_networks.py
+++ v3/neural_networks.py
@@ -5,11 +5,18 @@ from torch import nn
 
 class PolicyNetwork(nn.Module):
     """This is a class for implementing a neural network which inputs a board state, as -1s and 1s, and outputs a policy vector."""
-    
-    def __init__(self, inputSize: int=25, hiddenSize: int=25, outputSize: int=200):
+
+    def __init__(
+        self,
+        modelPath=None,
+        inputSize: int = 25,
+        hiddenSize: int = 25,
+        outputSize: int = 200,
+    ):
         """Initializes the PolicyNetwork class.
-        
+
         Args:
+            modelPath (str, optional): The path to the model file. Defaults to None.
             inputSize (int, optional): The size of the input layer. Defaults to 25.
             hiddenSize (int, optional): The size of the hidden layer. Defaults to 25.
             outputSize (int, optional): The size of the output layer. Defaults to 200.
@@ -23,65 +30,93 @@ class PolicyNetwork(nn.Module):
         # 10 11 12 13 14
         # 15 16 17 18 19
         # it is basically a row by row flattened version of the board
-        
-        
+
         super().__init__()
         self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
         self.flatten = nn.Flatten()
-        self.model = nn.Sequential(
-            nn.Linear(inputSize, hiddenSize),
-            nn.Tanh(),
-            nn.Linear(hiddenSize, outputSize),
-            nn.Softmax(dim=1)
-        ).to(self.device)
-        
+        if modelPath is not None:
+            self.model = nn.Sequential(
+                nn.Linear(inputSize, hiddenSize),
+                nn.Tanh(),
+                nn.Linear(hiddenSize, outputSize),
+                nn.Softmax(dim=1),
+            ).to(self.device)
+        else:
+            self.model = torch.load(modelPath).to(self.device)
+
     def forward(self, x):
         """This is a function which feeds the input through the neural network. It flattens the input first.
-        
+
         Args:
             x (torch.Tensor): The input tensor.
-        
+
         Returns:
             torch.Tensor: The output tensor.
         """
         x = x.to(self.device)
         x = self.flatten(x)
         return self.model(x)
-    
-    
+
+    def save(self, path):
+        """This is a function which saves the model.
+
+        Args:
+            path (str): The path to the model file.
+        """
+        torch.save(self.model, path)
+
+
 class ValueNetwork(nn.Module):
     """This is a class for implementing a neural network which inputs a board state, as -1s and 1s, and outputs The win probability for the player with the 1s."""
-    
-    def __init__(self, inputSize: int=25, hiddenSize: int=25, outputSize: int=1):
+
+    def __init__(
+        self,
+        modelPath=None,
+        inputSize: int = 25,
+        hiddenSize: int = 25,
+        outputSize: int = 1,
+    ):
         """Initializes the ValueNetwork class.
-        
+
         Args:
+            modelPath (str, optional): The path to the model file. Defaults to None.
             inputSize (int, optional): The size of the input layer. Defaults to 25.
             hiddenSize (int, optional): The size of the hidden layer. Defaults to 25.
             outputSize (int, optional): The size of the output layer. Defaults to 1.
         """
         # in the input layer, there are 25 neurons, one for each square on the board, and each neuron has a value of -1, 0, or 1
         # in the output layer, there is 1 neuron, and it has a value between 0 and 1
-        
+
         super().__init__()
         self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
         self.flatten = nn.Flatten()
-        self.model = nn.Sequential(
-            nn.Linear(inputSize, hiddenSize),
-            nn.Tanh(),
-            nn.Linear(hiddenSize, outputSize),
-            nn.Sigmoid()
-        ).to(self.device)
-        
+        if modelPath is not None:
+            self.model = nn.Sequential(
+                nn.Linear(inputSize, hiddenSize),
+                nn.Tanh(),
+                nn.Linear(hiddenSize, outputSize),
+                nn.Sigmoid(),
+            ).to(self.device)
+        else:
+            self.model = torch.load(modelPath).to(self.device)
+
     def forward(self, x):
         """This is a function which feeds the input through the neural network. It flattens the input first.
-        
+
         Args:
             x (torch.Tensor): The input tensor.
-        
+
         Returns:
             torch.Tensor: The output tensor.
         """
         x = x.to(self.device)
         x = self.flatten(x)
         return self.model(x)
+
+    def save(self, path):
+        """This is a function which saves the model.
+
+        Args:
+            path (str): The path to the model file.
+        """
+        torch.save(self.model, path)
